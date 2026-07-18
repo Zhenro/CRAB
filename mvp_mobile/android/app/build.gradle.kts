c30ebccd402bde608a1f1efa58de8ce7d272753b
Andres Fermin <111396192+Zhenro@users.noreply.github.com>
2026-07-17 21:15:57 -0400

    test

--- mvp_mobile/android/app/build.gradle.kts
+++ mvp_mobile/android/app/build.gradle.kts
@@ -9,9 +9,12 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
-    compileOptions {
+compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
     }
 
     defaultConfig {
